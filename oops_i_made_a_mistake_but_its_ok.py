5d0f9a790f811f2bf7c78f442d2b5c74329f53f9
Imaad747 <imaadtheawesome@gmail.com>
2025-04-16 18:59:09 +1000

    Add files via upload

---
+++ oops_i_made_a_mistake_but_its_ok.py
@@ -0,0 +1,42 @@
+"""class student:
+    def __init__(self,name,age,year,housecolour,teacher,classroom):
+        self.name=name
+        self.age=age
+        self.year=year
+        self.housecolour=housecolour
+        self.teacher=teacher
+        self.classroom=classroom
+        self.school="Antartica Penguin State Academy, School and University"
+    def the_penguins_of_the_antartica_university(self):
+        print(f"The Penguin's name is {self.name}.")
+        print(f"They are {self.age} years old.")
+        print(f"They are in grade/year {self.year}.")
+        print(f"They are in the house colour {self.housecolour}.")
+        print(f"Their teacher is {self.teacher}.")
+        print(f"They are in the classroom {self.classroom}.")
+        print(f"School: {self.school}.")
+
+p1=student("Pingu","8","3","F I S H","Mr Erebus","3F")
+#print (p1.the_penguins_of_the_antartica_university())
+p1.housecolour="E V I L  S E A L S"
+print (p1.housecolour)"""
+
+class fruits:
+    def __init__(self,name,country,colour,taste):
+        self.name=name
+        self.country=country
+        self.colour=colour
+        self.taste=taste
+    def something(self):
+        print(f"The fruit is called a {self.name}.")
+        print(f"The country it is from is {self.country}.")
+        print(f"The colour of the fruit is {self.colour}.")
+        print(f"The taste of the fruit is {self.taste}.")
+f1=fruits("Apple","Kazakhstan","Red or Green","Juicy")
+f1.something()
+        
+
+
+
+        
+        
