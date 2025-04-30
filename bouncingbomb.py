40043318f3089313f698ce16a0568d76c26188b3
Imaad747 <imaadtheawesome@gmail.com>
2025-04-30 18:35:10 +1000

    Add files via upload

--- bouncingbomb.py
+++ bouncingbomb.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-screen=pygame.display.set_mode((800,800))
+screen=pygame.display.set_mode((1200,800))
 pygame.display.set_caption("Bouncing Bomb")
 how_he_made_the_bomb=pygame.draw.circle(surface=screen,color="Red",center=[100,100],radius=60)
 speed=[1,1]
@@ -9,8 +9,8 @@ while True:
         if event.type==pygame.QUIT:
             exit()
     screen.fill("Black")
-    how_he_made_the_bomb.move(speed)
-    if how_he_made_the_bomb.left<0 or how_he_made_the_bomb.right>800:
+    how_he_made_the_bomb=how_he_made_the_bomb.move(speed)
+    if how_he_made_the_bomb.left<0 or how_he_made_the_bomb.right>1200:
         speed[0]=-speed[0]
     if how_he_made_the_bomb.top<0 or how_he_made_the_bomb.bottom>800:
         speed[1]=-speed[1]
