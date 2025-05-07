39ff07b56b21a1bf83cb5b53bac15b873184a8df
Imaad747 <imaadtheawesome@gmail.com>
2025-05-07 18:53:05 +1000

    Add files via upload

---
+++ paintbrush.py
@@ -0,0 +1,4 @@
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((1000,1000))
+colourpedia=["Red","Yellow","Orange","Green","White","Black","Purple","Dark Green","Pink","Blue","chocolate4"]
